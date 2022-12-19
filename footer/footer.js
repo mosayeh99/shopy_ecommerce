ba536295ea0ace90b9fa1b5afe53c1bc167c40f8
mosayeh99 <mohamedelsayeh66@gmail.com>
2022-12-19 20:57:47 +0200

    style footer contact section

--- footer/footer.js
+++ footer/footer.js
@@ -1,26 +1,31 @@
 window.addEventListener('load', () => {
-    // ------------------------Footer plus icon in small screen-------------------------
-    let aboutPlusIcon = document.querySelector('footer .about-plus-icon');
-    let contactPlusIcon = document.querySelector('footer .contact-plus-icon');
-    let locationPlusIcon = document.querySelector('footer .location-plus-icon');
-    
-    aboutPlusIcon.addEventListener('click', (e) => {
-        e.stopPropagation();
-        aboutPlusIcon.lastElementChild.classList.toggle('opened');
-        aboutPlusIcon.parentElement.nextElementSibling.classList.toggle('active');
+    // -----------------------Footer plus icon to show & hide content-------------------------
+    document.addEventListener('click', (e) => {
+        if (e.target.classList.contains('footer-plus-icon')) {
+            e.target.lastElementChild.classList.toggle('opened');
+            e.target.parentElement.nextElementSibling.classList.toggle('active');
+            if (e.target.classList.contains('contact-plus-icon')) {
+                setTimeout(() => {
+                    document.querySelector('footer .contact-filed').focus();
+                }, 100);
+            }
+        }
     })
 
-    contactPlusIcon.addEventListener('click', (e) => {
-        e.stopPropagation();
-        contactPlusIcon.lastElementChild.classList.toggle('opened');
-        contactPlusIcon.parentElement.nextElementSibling.classList.toggle('active');
-    })
+    // ------------------------Contact form input--------------------------
+    document.addEventListener('focus', (e) => {
+        if (e.target.classList.contains('contact-filed')) {
+            e.target.previousElementSibling.style.cssText = 'top:0px; color:white';
+        }
+    }, true);
 
-    locationPlusIcon.addEventListener('click', (e) => {
-        e.stopPropagation();
-        locationPlusIcon.lastElementChild.classList.toggle('opened');
-        locationPlusIcon.parentElement.nextElementSibling.classList.toggle('active');
-    })
+    document.addEventListener('blur', (e) => {
+        if (e.target.classList.contains('contact-filed')) {
+            if (e.target.value == "") {
+                e.target.previousElementSibling.style.cssText = 'top:20px; color:#777';
+            }
+        }
+    }, true);
 
     // ------------------------Set copyrights year-------------------------
     let copyrightYear = document.querySelector('footer .bottom-bar .getyear');
