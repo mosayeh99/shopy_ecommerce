27a08fadb2e09487678bc1a5f31cd2bac0b72f28
MahmoudEl-Sayed22 <malsayed752@gmail.com>
2022-12-27 22:19:36 +0200

    add features to addToCard

--- product/product.js
+++ product/product.js
@@ -45,8 +45,14 @@ fetch('https://raw.githubusercontent.com/mosayeh99/products_json_api/main/data/p
             <div class="display-size">
             `
         let productSize="";
+        let productSizeCounter = 0 ;
         e.sizes.forEach(elem => {
-            productSize += `<button>${elem}</button>`
+            if(productSizeCounter == 0){
+                productSize += `<button class="size active" data-productfirstsize="${elem}">${elem}</button>`
+                productSizeCounter++;
+            }else{
+                productSize += `<button class="size">${elem}</button>`
+            }
         })
         productQuantity = `
             </div>
@@ -65,7 +71,13 @@ fetch('https://raw.githubusercontent.com/mosayeh99/products_json_api/main/data/p
         productFoot=`
                 </ul>
             </div>
-            <button class="add-to-card-btn">Add to cart</button>
+            <button class="add-to-card-btn" data-productid="${e.id}" >Add to cart<span>
+                <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-check" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
+                <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+                <path d="M5 12l5 5l10 -10"></path>
+                </svg>
+            </span>
+            </button>
             <div class="product-features">
                 <div class="feature">
                     <div class="feature-img">
@@ -90,14 +102,20 @@ fetch('https://raw.githubusercontent.com/mosayeh99/products_json_api/main/data/p
         </div>
             `
             product =  productHead + imageSlider + productContent + productSize + productQuantity + productDescription + productFoot;
+
         }
     })
-    document.querySelector(".product-container").innerHTML = product;
-})
 
-setTimeout(() => {
-    let = mainProductImg = document.querySelector('.mainsliderimage img');
-    let = productThumbs = document.querySelectorAll('.slide-thumbs .slide-image');
+    document.querySelector(".product-container").innerHTML = product;
+    let decreaseQuantity = document.querySelector(".decrease-quantity")
+    let increaseQuantity = document.querySelector(".increase-quantity")
+    let quantity = document.querySelector(".quantity")
+    let addToCard = document.querySelector(".add-to-card-btn")
+    let productSizes =document.querySelectorAll(".size")
+    
+    
+    let mainProductImg = document.querySelector('.mainsliderimage img');
+    let productThumbs = document.querySelectorAll('.slide-thumbs .slide-image');
     document.addEventListener('click', (e) => {
         // e.stopPropagation();
         if (e.target.classList.contains('slide-image')) {
@@ -107,5 +125,46 @@ setTimeout(() => {
             e.target.classList.add('active');
             mainProductImg.src = e.target.src;
         }
+        
     })
-},1000)
+    
+    
+     increaseQuantity.addEventListener('click',function(){
+        quantity.innerHTML = Number(quantity.innerHTML) +1 ;
+    })
+    
+    decreaseQuantity.addEventListener('click',function(){
+        if (quantity.innerHTML !== "1"){
+        quantity.innerHTML = Number(quantity.innerHTML) -1 ;
+        }
+        
+    })
+    let sizeValue = document.querySelector(".display-size .size").dataset.productfirstsize;
+    let productsCartArray =[];
+
+    document.addEventListener('click',(e) =>{
+        if(e.target.classList.contains("size")){
+            productSizes.forEach(el =>{
+                el.classList.remove('active')
+            }) 
+            e.target.classList.add('active')
+            sizeValue = e.target.innerHTML;
+            // console.log(sizeValue)
+        }
+    })
+    addToCard.addEventListener('click',function(){
+        if (localStorage.productsInCard != null){
+        productsCartArray = JSON.parse(localStorage.productsInCard)
+        }
+        let productInfoObject = { 
+            productId : addToCard.dataset.productid ,
+            productSize : sizeValue ,
+            productQty : quantity.textContent
+        }
+        productsCartArray.push(productInfoObject);
+        localStorage.setItem('productsInCard',JSON.stringify(productsCartArray))
+        document.querySelector(".add-to-card-btn span").style.display = "inline"
+    })    
+})
+
+
