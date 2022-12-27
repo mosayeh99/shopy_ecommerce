a8dccabe7a459d4a18a4e76761943190e00bb2f1
mohamed.fci_1059 <mohamed.fci_1059@fci.kfs.edu.eg>
2022-12-27 18:08:37 +0200

    update code

---
+++ cart/cart.js
@@ -0,0 +1,122 @@
+let carts = document.querySelectorAll('.add-cart');
+let products = [
+  {
+    name: 'Rouge',
+    tag: '1',
+    price: 15,
+    inCart: 0
+  },
+  {
+    name: 'Sunglass',
+    tag: '2',
+    price: 20,
+    inCart: 0
+  },
+  {
+    name: 'Mobile',
+    tag: '3',
+    price: 30,
+    inCart: 0
+  },
+]
+
+for (let i=0; i < carts.length; i++){
+  carts[i].addEventListener('click', ()=> {
+    cartNumbers(products[i]);
+    totalCost(products[i]);
+  })
+}
+
+  function onLoadCartNumbers() {
+    let productNumbers = localStorage.getItem('cartNumbers');
+
+    if ( productNumbers) {
+      document.querySelector('.cart span').textContent = productNumbers;
+  }
+}
+  function cartNumbers(product) {
+    let productNumbers = localStorage.getItem('cartNumbers');
+
+    productNumbers = parseInt(productNumbers);
+
+    if ( productNumbers) {
+      localStorage.setItem('cartNumbers', productNumbers + 1);
+      document.querySelector('.cart span').textContent = productNumbers + 1;
+    } else{
+      localStorage.setItem('cartNumbers', 1);
+      document.querySelector('.cart span').textContent = 1;
+    }
+    setItems(product);
+  }
+
+  function setItems(product) {
+    let cartItems = localStorage.getItem('productInCart');
+    cartItems = JSON.parse(cartItems);
+    if (cartItems != null) {
+      if (cartItems[product.tag] == undefined) {
+        cartItems = {
+          ...cartItems,
+          [product.tag]: product
+        }
+      }
+      cartItems[product.tag].inCart += 1;
+    }else{
+    product.inCart = 1;
+    cartItems = {
+      [product.tag]: product
+     }
+  }
+    
+    localStorage.setItem("productInCart", JSON.stringify(cartItems));
+  }
+
+  function totalCost(product) {
+    let cartCost = localStorage.getItem('totalCost');
+
+    if (cartCost != null) {
+      cartCost = parseInt(cartCost);
+      localStorage.setItem("totalCost", cartCost + product.price);
+    }else{
+    localStorage.setItem("totalCost", product.price);
+  }
+}
+
+function displayCart() {
+  let cartItems = localStorage.getItem("productInCart");
+  cartItems = JSON.parse(cartItems);
+
+  let productContainer = document.querySelector(".products");
+  let cartCost = localStorage.getItem('totalCost');
+
+  if (cartItems && productContainer ) {
+    productContainer.innerHTML = '';
+    Object.values(cartItems).map(item => {
+      productContainer.innerHTML += `
+      <div class="product">
+      <ion-icon name="close-circle-outline"></ion-icon>
+        <img src="./img/${item.tag}.jpg">
+        <span> ${item.name} </span>
+      </div>
+      <div class="price"> $${item.price},00 </div>
+      <div class="quantity">
+      <ion-icon name="caret-back-circle-outline"></ion-icon>
+      <span> ${item.inCart} </span>
+      <ion-icon name="caret-forward-circle-outline"></ion-icon>
+      </div>
+      <div class="total"> $${item.inCart * item.price},00 </div>
+      `;
+    });
+      productContainer.innerHTML += `
+      <div class="basketTotalContainer">
+        <h4 class="basketTotalTitle">
+              Basket Total
+        </h4>
+        <h4 class="basketTotal">
+          $ ${cartCost},00
+        </h4>
+      </div>
+    `;
+  }
+}
+onLoadCartNumbers();
+displayCart();
