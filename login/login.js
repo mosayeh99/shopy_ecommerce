7f659f832d3131ebb1944031fd9501f34a30ab32
mosayeh99 <mohamedelsayeh66@gmail.com>
2022-12-31 22:04:35 +0200

    link pages with user in localstorage

--- login/login.js
+++ login/login.js
@@ -1,211 +1,174 @@
-
-
-// localStorage.clear()
-
+let arr = [];
+if (localStorage.User != null) {
+    arr =JSON.parse(localStorage.getItem("User"));
+}
+// localStorage.setItem("User",JSON.stringify(arr))
 // ------------------Password Showing ---------------------
 var passlog = document.getElementById('passLog') ;
 var showlog = document.getElementById('showLog') ;
-
 showlog.addEventListener('click' , function(){
-    
     if(passlog.type === "password"){
-            passlog.type= "text";
-        }
-        else{
-            passlog.type = "password" ;
-        }
-    }) ;
-    
-    var passreg = document.getElementById('passReg') ;
+        passlog.type= "text";
+    }else{
+        passlog.type = "password" ;
+    }
+}) ;
+
+var passreg = document.getElementById('passReg') ;
 var showreg = document.getElementById('showReg') ;
+showreg.addEventListener('click' , function(){
+    if(passreg.type === "password"){
+        passreg.type= "text";
+    }
+    else{
+        passreg.type = "password" ;
+    }
+}) ;
 
-    showreg.addEventListener('click' , function(){
+var newPass = document.getElementById('newPass') ;
+var showReset = document.getElementById('showReset') ;
+showReset.addEventListener('click' , function(){
+    if(newPass.type === "password"){
+        newPass.type= "text";
+    }else{
+        newPass.type = "password" ;
+    }
+}) ;
 
-        if(passreg.type === "password"){
-            passreg.type= "text";
-        }
-        else{
-            passreg.type = "password" ;
-        }
-    }) ;
-    
-    var newPass = document.getElementById('newPass') ;
-    var showReset = document.getElementById('showReset') ;
-    
-    showReset.addEventListener('click' , function(){
-        
-        if(newPass.type === "password"){
-            newPass.type= "text";
-        }
-        else{
-            newPass.type = "password" ;
-        }
-    }) ;
+// ------------------------------- Login Form ------------------------------- //
+document.getElementById("loginNow").addEventListener("click",function(){
+    document.getElementById("login").style.display = "block"
+    document.getElementById("signup").style.display = "none"
+    document.getElementById("reset").style.display = "none"
+    window.scrollTo({
+        top: 0,
+        behavior: "smooth"
+    })
+})
 
-
-    
-    // ------------------------------- Login Form ------------------------------- //
-
-
-    document.getElementById("logform").addEventListener('click',function(){
-        
-    
+document.getElementById("logBtn").addEventListener('click',function(e){
+    if (arr.length != 0) {
         var userLog = document.getElementById("userLog").value;
         var passLog = document.getElementById("passLog").value;
-        var userReg= (JSON.parse(localStorage.getItem("User"))[0].RegUser) ;
-        var passReg= (JSON.parse(localStorage.getItem("User"))[0].RegPassword) ;
-    
-
-
-        if (userLog === userReg && passLog === passReg){
-            Swal.fire({
-                icon: 'success',
-                title: 'Successfully Login',
-                text: 'Wish you a good Shopping ',
-                footer: `<a href="../index.html">  Home Page  /</a>
-                         <a href="../checkout/checkout.html"> Checkout  /</a>
-                         <a href="./login.html">  LogOut  </a>`
-                        })
-                        document.getElementById("error").style.display = "none"
-                    document.getElementById("logform").style.display ="none"
-        }
-   
-        document.getElementById("passLog").addEventListener('blur', function(){
-         if ((userLog !== userReg && passLog !== passReg)){
-            
-            document.getElementById("error").innerHTML ="Wrong e-mail <br> Retry Or Register Now"
-            
+        var userReg= arr[0].RegUser;
+        var passReg= arr[0].RegPassword;
+        if (userLog == "") {
+            e.preventDefault();
+            document.getElementById("error").innerHTML ="Please enter your Email.";
             document.getElementById("error").style.display = "block"
-        }
-
-        else if ((userLog === userReg && passLog !== passReg )){
-            document.getElementById("error").innerHTML ="Incorrect Password"
+        }else if (passLog == "") {
+            e.preventDefault();
+            document.getElementById("error").innerHTML ="Please enter your password.";
             document.getElementById("error").style.display = "block"
+        }else if (userLog != userReg && passLog != passReg){
+            e.preventDefault();
+            document.getElementById("error").innerHTML ="Your email or password is incorect."
+            document.getElementById("error").style.display = "block"
+        }else {
+            document.getElementById("error").style.display = "none"
+            // -------------Shopy Alert success---------------
+            document.querySelector('#shopy-alert-overlay').classList.add('active');
+            document.querySelector('#shopy-alert-success').classList.add('active');
+            setTimeout(() => {
+                history.back();
+            }, 3000);
         }
-        })
+    }else {
+        e.preventDefault();
+        document.getElementById("error").innerHTML ="Your email or password is incorect."
+        document.getElementById("error").style.display = "block"
+    }
+})
 
-        // document.getElementById("passLog").addEventListener('blur', function(){
-        //     if (userLog === userReg && passLog !== passReg){
-                
-        //         document.getElementById("error").innerHTML ="Incorrect Password"
-        //         document.getElementById("error").style.display = "block"
-        //     }})
-    })
-        
+// ------------------------------- Regestration Form ------------------------------- //
+document.getElementById("RegisterNow").addEventListener("click",function(){
+    document.getElementById("login").style.display = "none"
+    document.getElementById("signup").style.display = "block"
+    document.getElementById("reset").style.display = "none"
+})
 
+var signupBtn = document.getElementById("signupBtn");
+var fname = document.getElementById("fname");
+var lname = document.getElementById("lname");
+var addr   = document.getElementById("addr");
+var userReg = document.getElementById("userReg");
+var passReg = document.getElementById("passReg");
 
+signupBtn.addEventListener("click",function(e){
+    e.preventDefault();
+    if (userReg.value.match(/@\w+.\w+/ig) != null){
 
-    // ------------------------------- Regestration Form ------------------------------- //
-    
-    document.getElementById("RegisterNow").addEventListener("click",function(){
-        
-        
-        document.getElementById("login").style.display = "none"
-        document.getElementById("signup").style.display = "block"
-        document.getElementById("reset").style.display = "none"
-    })
-        
-        
-        var signupBtn = document.getElementById("signupBtn");
-        signupBtn.addEventListener("click",function(){
-            
-            
-            var fname = document.getElementById("fname").value;
-            var lname = document.getElementById("lname").value;
-            var addr   = document.getElementById("addr").value ;
-            var userReg = document.getElementById("userReg").value;
-            var passReg = document.getElementById("passReg").value;
-            
-            let arr = []
+    }
+})
 
-        if (isNaN (userReg) && passReg.length >=8){
-            document.getElementById("signupBtn").style.pointerEvents ="auto"
-            var obj ={
-                "Address" :  [addr] ,
-                "RegPassword" : passReg ,
-                "RegUser" : userReg ,
-                "LastName" :lname ,
-                "FirstName" : fname ,
-                "login" : true 
-            }
-            arr.push(obj)
-
-            localStorage.setItem("User",JSON.stringify(arr)) 
-
-            document.getElementById("regError").innerHTML =""
-            document.getElementById("regError").style.display = "none"
-            
-        }
-        else if(!isNaN (userReg) && passReg.length <8 ) {
-
-            document.getElementById("regError").innerHTML ="Email is invalid <br>Password is too short (minimum is 8 characters) "
-            document.getElementById("regError").style.display = "block"
-
-            }
-        else if(!isNaN (userReg)) {
-                document.getElementById("regError").innerHTML ="Email is invalid"
-            }
-        else if(passReg.length <8 ) {
-                document.getElementById("regError").innerHTML ="Password is too short (minimum is 8 characters)"
+signupBtn.addEventListener("click",function(e){
+    let emailExist = false;
+    if (arr.length != 0) {
+        arr.forEach((el) => {
+            if (userReg.value == el.RegUser) {
+                document.getElementById("regError").innerHTML ="Email is already registered!"
+                document.getElementById("regError").style.display = "block"
+                emailExist = true;
             }
         })
-    
-    
+    }
+    if (fname.value == "" || lname.value == "" || addr.value == "" || userReg.value == "" || passReg.value == "") {
+        e.preventDefault();
+        document.getElementById("regError").innerHTML ="There are empty fields"
+        document.getElementById("regError").style.display = "block"
+    }else if(userReg.value.match(/@\w+.\w+/ig) == null) {
+        e.preventDefault();
+        document.getElementById("regError").innerHTML ="Email is invalid"
+        document.getElementById("regError").style.display = "block"
+    }else if (userReg.value.match(/@\w+.\w+/ig) != null && emailExist == true){
+        e.preventDefault();
+    }else if(passReg.value.length <8) {
+        e.preventDefault();
+        document.getElementById("regError").innerHTML ="Password is too short (minimum is 8 characters)"
+        document.getElementById("regError").style.display = "block"
+    }else {
+        console.log('iam in')
+        var obj ={
+            "Address" :  [addr.value] ,
+            "RegPassword" : passReg.value ,
+            "RegUser" : userReg.value ,
+            "FirstName" : fname.value ,
+            "LastName" :lname.value ,
+            "loginStatus" : true
+        }
+        arr.push(obj);
+        localStorage.setItem("User",JSON.stringify(arr));
+        history.back();
+    }
+})
 
-    // ------------------------------- Reset Password Form ------------------------------- //
-        document.getElementById("return").addEventListener("click",function() {
+userReg.addEventListener('blur', () => {
+    if (userReg.value.match(/@\w+.\w+/ig) == null) {
+        document.getElementById("regError").innerHTML ="Email is invalid"
+        document.getElementById("regError").style.display = "block"
+    }
+}, true);
+passReg.addEventListener('blur', () => {
+    if(passReg.value.length <8) {
+        document.getElementById("regError").innerHTML ="Password is too short (minimum is 8 characters)"
+        document.getElementById("regError").style.display = "block"
+    }
+}, true)
 
-            document.getElementById("login").style.display = "block"
-            document.getElementById("signup").style.display = "none"
-            document.getElementById("reset").style.display = "none"
-           })
-    
-    document.getElementById("forget").addEventListener("click",function(){
-        
-        document.getElementById("login").style.display = "none"
-        document.getElementById("signup").style.display = "none"
-        document.getElementById("reset").style.display = "block"
-
-        document.getElementById("ResetBtn").addEventListener("click", function(){
-            newPass = document.getElementById("newPass").value 
-            let arr =JSON.parse(localStorage.getItem("User"))
-            var fname = arr[0].FirstName ;
-            var lname = arr[0].LastName ;
-            var addr   = arr[0].Address  ;
-            var userReg = arr[0].RegUser ;
-            
-            if(newPass.length >=8){        
+// ------------------------------- Reset Password Form ------------------------------- //
+document.getElementById("forget").addEventListener("click",function(){
+    document.getElementById("login").style.display = "none"
+    document.getElementById("signup").style.display = "none"
+    document.getElementById("reset").style.display = "block"
+    document.getElementById("ResetBtn").addEventListener("click", function(e){
+        newPass = document.getElementById("newPass").value;
+        if (newPass.length <8){
+            e.preventDefault();
+            document.getElementById("resetError").innerHTML ="Password is too short (minimum is 8 characters)";
+            document.getElementById("resetError").style.display = "block"
+        }else if (newPass.length >=8){
             arr[0].RegPassword = newPass ;
-
-            localStorage.setItem('User',JSON.stringify(arr)) ;
-            var obj ={
-                "Address" :  [addr] ,
-                "RegPassword" : newPass ,
-                "RegUser" : userReg ,
-                "LastName" :lname ,
-                "FirstName" : fname ,
-                "login" : true 
-            }
-            arr.push(obj)   
-            }
-            
-            else if (newPass.length <8){
-                document.getElementById("resetError").innerHtml ="Uncorrect Password"
-            }
-            
-            
-        })
-
+            localStorage.setItem('User',JSON.stringify(arr));
+        }
     })
-
-//     let arr =JSON.parse(localStorage.getItem("User"))
-
-//     while (arr.login === true)
-//     {
-
-//     document.getElementById("logform").style.display = "none";
-
-//     }
-
-
-//    console.log(arr[0].login)
+})
