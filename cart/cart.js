09c34e854517401cf92fc259c111fdce48993654
mosayeh99 <mohamedelsayeh66@gmail.com>
2022-12-31 22:08:46 +0200

    link page with users in localstorage

--- cart/cart.js
+++ cart/cart.js
@@ -122,3 +122,23 @@ document.addEventListener('click', (e) => {
         });
     }
 })
+
+let allUsersArray = [];
+if (localStorage.User != null) {
+    allUsersArray = JSON.parse(localStorage.User);
+}
+let ProceedToCheckoutBtn = document.querySelector('#cart-foot-checkout-btn');
+ProceedToCheckoutBtn.addEventListener('click', () => {
+    let userLoginCounter;
+    allUsersArray.forEach((el) => {
+        console.log(el.loginStatus);
+        userLoginCounter = 0;
+        if (el.loginStatus == true) {
+            location.href = '../checkout/checkout.html';
+            userLoginCounter++;
+        }
+    })
+    if (userLoginCounter == 0) {
+        location.href = '../login/login.html';
+    }
+})
